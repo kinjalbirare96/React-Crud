9f9fed51af5744ca892b5ccb187d74912642d29f
Admin <kinjal@visibly.io>
2019-06-04 01:06:18 +0530

    add update delete

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,9 @@ import CKEditor from '@ckeditor/ckeditor5-react';
 import classicEditor from '@ckeditor/ckeditor5-build-classic';
 
 var Allvalueonfeild = [];
+var itemindex = '';
+var item = "";
+var reseteditor = false;
 class Home extends React.Component {
 
     constructor(props) {
@@ -13,30 +16,37 @@ class Home extends React.Component {
             address: '',
             profile : '',
             editorData: '',
-            ListArray : null
+            ListArray : null,
+            updateFlag : false,
         };
       }
   handleSubmit(){
-        console.log("hi");
-        Allvalueonfeild.push({
-             'name' :    this.state.name , 
-             'address' : this.state.address,
-             'profile': this.state.profile,
-             'editor' : this.state.editorData
-        })
-        this.setState({
+
+    reseteditor = true 
+       if(this.state.updateFlag == false) {
+            Allvalueonfeild.push({
+                'name' :    this.state.name , 
+                'address' : this.state.address,
+                'profile': this.state.profile,
+                'editor' : this.state.editorData
+           })
+      }else{
+            console.log( Allvalueonfeild[itemindex]  , " Allvalueonfeild[itemindex] ");
+            console.log(item , "item");
+            Allvalueonfeild[itemindex].name = this.state.name;
+            Allvalueonfeild[itemindex].address = this.state.address;
+            Allvalueonfeild[itemindex].profile = this.state.profile;
+            Allvalueonfeild[itemindex].editor = this.state.editorData;
+
+        }
+      this.setState({
             ListArray : Allvalueonfeild ,
             name : '',
             address :'',
             profile:'',
-            editor:''
-        });
-     }
-     updateDataValue(e,index){
-       console.log("_+++++",index);
-       console.log(this.state.ListArray);
-       var item =  this.state.ListArray.findIndex(index);
-       console.log(item, "iem");
+            editor:'',
+            updateFlag:false
+        }); 
      }
     componentDidMount(){
         const script = document.createElement("script");
@@ -61,69 +71,70 @@ class Home extends React.Component {
                })
         }
     }
+    updateDataValue(index ,e){
+        itemindex = index;
+        item = this.state.ListArray[index];
+        this.setState({
+            name : item.name,
+            address : item.address,
+            //profile: item.profile,
+            editor : item.editor,
+            updateFlag:true
+        })
+  }
+ 
  render() {
-       console.log(this.state.ListArray)
       return (
-           <div className= "information-wrapper-container"> 
-               <h1 className = "title">  Information  </h1>
-               <div className = "form-container"> 
-                 <form onSubmit={this.handleSubmit}> 
-                     <div className = "form-row">
-                           <label> Name : </label> 
-                           <input type = "text"  name = "name"  onChange = {this.handleOnchange.bind(this, 'name')} value = {this.state.name} />
-                     </div>
-                     <div className = "form-row">
-                           <label> Address : </label> 
-                           <textarea row= "5" name = "Address"  onChange = {this.handleOnchange.bind(this, 'address')}  value = {this.state.address} />
-                     </div>
-                     <div className = "form-row">
-                           <label> Profile-pic : </label> 
-                            <div className = "file-container"> 
-                                <input type = "file" name = "file"  onChange = {this.handleOnchange.bind(this, 'profile')}  value = {this.state.profile}/>
-                            </div>
-                     </div>
-                     <div className = "form-row">
-                           <label> Web development : </label> 
-                            <div className = "editor"> 
-                            <CKEditor
-                                editor={ classicEditor }
-                                data="<p>Hello from CKEditor 5!</p>"
-                                onInit={ editor => {
-                                    // You can store the "editor" and use when it is needed.
-                                    console.log( 'Editor is ready to use!', editor );
-                                } }
-                                onChange={ ( event, editor ) => {
-                                    const data = editor.getData();
-                                    this.setState( {
-                                           editorData : data     
-                                      })
-                                    console.log( { event, editor, data } );
-                                } }
-                                onBlur={ editor => {
-                                    console.log( 'Blur.', editor );
-                                } }
-                                onFocus={ editor => {
-                                    console.log( 'Focus.', editor );
-                                } }
-                            />
-                            </div>
-                     </div>
-                     <div className = "form-row btn-container">
-                           <input type = "button" className = "btn" onClick = {this.handleSubmit.bind(this)} value = "Submit"/>
-                     </div>
-                      
-                 </form>
-               </div>
+        <div className= "information-wrapper-container">
+        <h1 className = "title">  Information  </h1>
+        <div className = "form-container">
+           <form onSubmit={this.handleSubmit}>
+              <div className = "form-row">
+                 <label> Name : </label> 
+                 <input type = "text"  name = "name"  onChange = {this.handleOnchange.bind(this, 'name')} value = {this.state.name} />
+              </div>
+              <div className = "form-row">
+                 <label> Address : </label> 
+                 <textarea row= "5" name = "Address"  onChange = {this.handleOnchange.bind(this, 'address')}  value = {this.state.address} />
+              </div>
+              <div className = "form-row">
+                 <label> Profile-pic : </label> 
+                 <div className = "file-container"> 
+                    <input type = "file" name = "file"  onChange = {this.handleOnchange.bind(this, 'profile')}  value = {this.state.profile}/>
+                 </div>
+              </div>
+              <div className = "form-row">
+                 <label> Web development : </label> 
+                 <div className = "editor">
+                    <CKEditor
+                       editor={ classicEditor }
+                       data="<p>Hello  Kinjal Birare</p> "
+                       
+                       onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        this.setState( {
+                            editorData : data     
+                            })
+                    } }
+                    
+                    />
 
-               <div className  = "list-container"> 
-                  {this.state.ListArray != null && this.state.ListArray != '' &&  this.state.ListArray != undefined ? 
-                    <List 
-                    feildValue = {this.state.ListArray}
-                    updateValue = {this.updateDataValue.bind(this.index)}
-                     />
-                    :''}
-               </div>
-           </div>
+                 </div>
+              </div>
+              <div className = "form-row btn-container">
+                 <input type = "button" className = "btn" onClick = {this.handleSubmit.bind(this)} value = "Submit"/>
+              </div>
+           </form>
+        </div>
+        <div className  = "list-container">
+           {this.state.ListArray != null && this.state.ListArray != '' &&  this.state.ListArray != undefined ? 
+           <List 
+              feildValue = {this.state.ListArray}
+              updateDataValue = {this.updateDataValue.bind(this)}
+              />
+           :''}
+        </div>
+     </div>
       )
     }
   }
--- src/List.js
+++ src/List.js
@@ -1,23 +1,19 @@
 import React from 'react';
 const List = props =>(
-<div class = "listContainer">
+<div className = "listContainer">
    <ul>
-      {console.log(props.feildValue)}
       {props.feildValue.map((item,index) => (
          
                 <li key = {index}  id={`list-${index}`}>  
-                     <input type = "button" onClick = {props.updateValue(this,index)} class = "send-button" value = "update"/>
+                    <span className = "input-container"><input type = "button" onClick = {props.updateDataValue.bind(this,index)} class = "send-button" value = "update"/></span>
                     <span> Name : {item.name} </span>
                     <span>Address :  {item.address}</span>
                     <span> Profile Url : {item.profile}</span>
                     <span>editor Data : {item.editor}</span>
                </li>
         ))}
-      
-    
       </ul>
-
-  </div>
+ </div>
 )
 
 export default List;
